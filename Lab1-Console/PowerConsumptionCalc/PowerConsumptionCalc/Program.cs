898b091540b7a426d16dfedc4d72675df49b90a6
CP2047\Taras <taras.hrechyn@hotmail.com>
2017-09-24 21:46:22 +0300

    L1 Console Application Title Added

--- Lab1-Console/PowerConsumptionCalc/PowerConsumptionCalc/Program.cs
+++ Lab1-Console/PowerConsumptionCalc/PowerConsumptionCalc/Program.cs
@@ -7,11 +7,12 @@ namespace PowerConsumptionCalc
         static void Main(string[] args)
         {
             // організація циклу для читання та опрацювання коду натисненої клавіші
+            Console.Title = "Console Demo App";
             bool programTerminated = false;
             while (!programTerminated)
             {
                 Console.Clear();
-                Console.WriteLine("Виберіть пункт меню: ");
+                Console.WriteLine("Виберiть пункт меню: ");
                 Console.WriteLine("F1 - Допомога");
                 Console.WriteLine("F2 - Розрахунок довжини кола");
                 Console.WriteLine("Esc - завершення роботи програми");
@@ -23,22 +24,22 @@ namespace PowerConsumptionCalc
                         Console.WriteLine("\nТут буде детальніша інформація...\n");
                         break;
                     case ConsoleKey.F2:
-                        Console.WriteLine("\nЗадайте радіус кола: ");
+                        Console.Write("\nЗадайте радiус кола: ");
                         string radiusStr = Console.ReadLine();
                         double radius = Convert.ToDouble(radiusStr);
                         double constantaPi = 3.14;
                         double length = 2 * constantaPi * radius;
-                        Console.WriteLine("Довжина кола: {0}\n", length);                        
+                        Console.WriteLine("\nДовжина кола: {0}\n", length);                        
                         break;
                     case ConsoleKey.Escape:
-                        programTerminated = true;
-                        break;
+                        programTerminated = true;                        
+                        continue;
                     default:
                         Console.WriteLine("\nНевідома операція\n");
                         break;
                 }
                 // Затримка перед наступним сеансом роботи
-                Console.WriteLine("Натисніть довільну клавішу");
+                Console.WriteLine("Натиснiть довiльну клавiшу");
                 Console.ReadKey();
             }
         }
