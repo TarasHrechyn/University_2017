a113d95e677ec10080e3f072415db420de9621ec
CP2047\Taras <taras.hrechyn@hotmail.com>
2017-12-01 21:09:31 +0200

    WEB Initial commit

---
+++ Lab1-Console/PowerConsumptionCalc/PowerCalcWeb/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PowerCalcWeb.Startup))]
+namespace PowerCalcWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
